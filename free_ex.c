bc9d10aaf0200278d2e842078ad1afc3b878a156
diego0096 <dfqz93@hotmail.com>
2020-01-04 23:09:59 +0000

    executes commit

--- free_ex.c
+++ free_ex.c
@@ -1,29 +1,31 @@
 #include "monty.h"
 
 /**
- *free_ex - free allocated memory at exit
+ *free_ex - free allocated memory.
+ *
  *Description: free memory
  */
 void free_ex(void)
 {
-    free_stack_t(global.stack);
-    fclose(global.file);
-    if (global.line)
-        free(global.line);
+	free_stack_t(global.stack);
+	fclose(global.file);
+	if (global.line)
+		free(global.line);
 }
 
 /**
  *free_stack_t - free top of the stack
+ *
  *@top: top of the stack
  */
 void free_stack_t(stack_t *top)
 {
-    stack_t *t;
+	stack_t *t;
 
-    while (top != NULL)
-    {
-        t = top;
-        top = top->next;
-        free(t);
-    }
+	while (top != NULL)
+	{
+		t = top;
+		top = top->next;
+		free(t);
+	}
 }
